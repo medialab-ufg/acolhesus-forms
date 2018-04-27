b81cf32d87c1c0a7e8e0bca56757c7ae2b835ea3
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-04-27 18:50:37 -0300

    Passa lista de forms para estrutura de tabela

--- templates/loop-forms.php
+++ templates/loop-forms.php
@@ -1,37 +1,45 @@
-<h1 class="acolhesus-archive-title">
-    <?php echo post_type_archive_title('Formulários: '); ?>
-</h1>
-<?php
-if ( have_posts() ) {
-    while(have_posts()): the_post();
-    $author_id = get_the_author_meta( 'ID' );
-    ?>
-        <div class="panel panel-default col-md-3">
-            <div class="panel-body">
-                <a href="<?php the_permalink(); ?>">
-                    <?php the_title( '<h3 class="panel-title">', '</h3>' ); ?>
-                </a>
-            </div>
-            <div class="panel-footer">
-                <span class="post-date text-uppercase"> <span style="font-size: 10px"> Publicado em </span> <?php the_time( 'd/m/Y' ); ?></span>
-                <br>
-                <span class="text-uppercase" style="font-size: 10px"> por </span>  <span class="nome-author"> <a href="<?php echo get_author_posts_url($author_id); ?>"><?php echo get_the_author(); ?></a> </span>
-            </div>
-        </div>
-        <!-- .panel .panel-default .col-md-3-->
-        <div style="margin-left: 1%; float: left; position: relative; width: 10px; height: 10px;"></div>
-    <?php
-    endwhile;
-}
+<div class="acolhesus-form-container col-md-12">
+    <h1 class="acolhesus-archive-title"> <?php echo post_type_archive_title('Formulários: '); ?> </h1>
+    <table class="table table-condensed">
+        <thead>
+        <tr>
+            <th> Nome </th>
+            <th> Data Resposta </th>
+            <th> Autor </th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        if ( have_posts() ) {
+            while(have_posts()): the_post();
+            $author_id = get_the_author_meta( 'ID' );  ?>
+                <tr>
+                    <td>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_title( '<h3 class="panel-title">', '</h3>' ); ?>
+                        </a>
+                    </td>
+                    <td> <?php the_time( 'd/m/Y' ); ?> </td>
+                    <td> <a href="<?php echo get_author_posts_url($author_id); ?>"><?php echo get_the_author(); ?></a> </td>
+                </tr>
+            <?php
+            endwhile;
+        }
+        ?>
+        </tbody>
+    </table>
 
-$_post_type = get_post_type();
- if ($AcolheSUS->can_add_entry($_post_type)) { ?>
-     <div class="col-md-12">
-         <p>
-             <input type="hidden" id="new_post_type" name="new_post_type" value="<?php echo $_post_type; ?>">
-             <button class="btn btn-info" id="add_acolhesus_entry">Nova resposta de <?php echo post_type_archive_title(); ?> </button>
-         </p>
-     </div>
-     <?php
- }
-?>
+    <?php
+    $_post_type = get_post_type();
+    if ($AcolheSUS->can_add_entry($_post_type)) { ?>
+        <div class="col-md-12">
+            <p>
+                <input type="hidden" id="new_post_type" name="new_post_type" value="<?php echo $_post_type; ?>">
+                <button class="btn btn-info" id="add_acolhesus_entry">Nova resposta de <?php echo post_type_archive_title(); ?> </button>
+            </p>
+        </div>
+        <?php
+    }
+    ?>
+
+</div>
