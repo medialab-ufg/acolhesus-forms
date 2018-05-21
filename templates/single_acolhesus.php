875eeb4f8e89dc833f03988693ae84836c7b274f
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-05-21 11:30:57 -0300

    Fix botão "ver todas as respostas" nao clicavel

--- templates/single_acolhesus.php
+++ templates/single_acolhesus.php
@@ -9,9 +9,7 @@
         <?php endif; ?>
 
         <?php if (current_user_can('acolhesus_cgpnh')): ?>
-            <button class="btn btn-default" style="float: right; background: #003c46">
-                <a style="color: white" href="<?php echo get_post_type_archive_link(get_post_type()); ?>"> VER TODAS RESPOSTAS </a>
-            </button>
+            <a class="btn btn-default list-entries" href="<?php echo get_post_type_archive_link(get_post_type()); ?>"> VER TODAS RESPOSTAS </a>
         <?php endif; ?>
     </h3>
 
