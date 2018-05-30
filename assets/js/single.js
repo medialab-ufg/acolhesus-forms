9fdc36b2917b263912c5a1c77dac4bda96991668
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-05-30 14:46:21 -0300

    Melhora escopo js para forms fechados

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,7 @@
 jQuery( function( $ ) {
     var no_edit = '.acolhesus-readonly';
     var base = '.acolhesus-form-container';
+    var cant_edit = ($(".acolhesus-form-container " + no_edit).length > 0);
 
     $('.acolhesus_basic_info_selector').change(function() {
         $.post(acolhesus.ajax_url, {
@@ -12,38 +13,40 @@ jQuery( function( $ ) {
         })
     }).change();
 
-    $(base + ' ' + no_edit + ' input[type=\'submit\']').remove();
-    $(no_edit + ' :radio').attr('disabled', true);
-    $(no_edit + ' :checkbox').attr('disabled', true);
-    $(no_edit + ' a.acolhesus_readonly').each( function(id, e) {
-        var answer = $(this).text();
-        $(this).replaceWith(answer);
-    });
-    $(no_edit + ' .is-cfdatepicker').each( function(id, e) {
-        var date = $(this).val();
-        $(this).replaceWith(date);
-    });
-
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            var new_elements = mutation.addedNodes;
-            if (!new_elements)
-                return;
-
-            for (var i = 0; i < new_elements.length; i++) {
-                var _el = new_elements[i];
-                if ('trumbowyg-button-pane' === $(_el).attr('class')) {
-                    $(_el).remove();
-                    $(no_edit + ' .trumbowyg-editor').attr('contenteditable', false);
-                }
-            }
+    if(cant_edit) {
+        $(base + ' ' + no_edit + ' input[type=\'submit\']').remove();
+        $(no_edit + ' :radio').attr('disabled', true);
+        $(no_edit + ' :checkbox').attr('disabled', true);
+        $(no_edit + ' a.acolhesus_readonly').each( function(id, e) {
+            var answer = $(this).text();
+            $(this).replaceWith(answer);
         });
-    }).observe(document.body, {
-        childList: true,
-        subtree: true,
-        attributes: false,
-        characterData: false
-    });
+        $(no_edit + ' .is-cfdatepicker').each( function(id, e) {
+            var date = $(this).val();
+            $(this).replaceWith(date);
+        });
+
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                var new_elements = mutation.addedNodes;
+                if (!new_elements)
+                    return;
+
+                for (var i = 0; i < new_elements.length; i++) {
+                    var _el = new_elements[i];
+                    if ('trumbowyg-button-pane' === $(_el).attr('class')) {
+                        $(_el).remove();
+                        $(no_edit + ' .trumbowyg-editor').attr('contenteditable', false);
+                    }
+                }
+            });
+        }).observe(document.body, {
+            childList: true,
+            subtree: true,
+            attributes: false,
+            characterData: false
+        });
+    }
 
     if ($('.user-cant-see').length > 0) {
         $(base + ' h3').remove();
