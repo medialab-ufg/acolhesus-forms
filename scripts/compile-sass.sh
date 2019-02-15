db81e96370b15c5ae2f202dd0fb57188eeca196f
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2019-02-14 23:36:28 -0200

    Cria opção no script de compilar o sass observando as alterações, facilitando desenvolvimento

--- scripts/compile-sass.sh
+++ scripts/compile-sass.sh
@@ -3,5 +3,12 @@
 cd ../assets/sass
 pwd
 echo "Compilando SASS do plugin AcolheSUS ..."
-sass index.scss:../css/acolhesus.css
-echo "...Pronto!"
+
+if [ "$1" == "w" ]
+then
+    echo "Observando alterações no código ..."
+    sass --watch index.scss:../css/acolhesus.css
+else
+	sass index.scss:../css/acolhesus.css
+	echo "...Pronto!"
+fi
