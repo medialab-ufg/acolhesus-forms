fad61765a78a7210591113b8f80850862604cedd
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2019-03-08 22:56:24 -0300

    Finishes most basic tests (resolves #99)

--- tests/test-acolhesus-common.php
+++ tests/test-acolhesus-common.php
@@ -11,7 +11,24 @@
 class AcolheSUSCommonTest extends WP_UnitTestCase {
     use AcolheSUSCommon;
 
+    // Adaptar teste sempre/quando estados entrarem/saírem
+    private $CURRENT_ACTIVE_STATES = 16;
+    private $TOTAL_FASES = 4;
+    private $TOTAL_EIXOS = 5;
+
 	public function test_get_acolhesus_title() {
 		$this->assertEquals('Plataforma de Gestão AcolheSUS', $this->get_title() );
 	}
+
+	public function test_has_16_active_states() {
+	    $this->assertCount($this->CURRENT_ACTIVE_STATES, $this->campos_completos);
+    }
+
+    public function test_has_4_phases() {
+        $this->assertCount($this->TOTAL_FASES, $this->fases);
+    }
+
+    public function test_has_5_axes() {
+        $this->assertCount($this->TOTAL_EIXOS, $this->eixos);
+    }
 }
