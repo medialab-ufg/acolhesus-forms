a7e6a574c0361fe7596bcdfb712d6ac015a1d796
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2019-03-11 00:37:17 -0300

    Better test class setup

--- tests/test-acolhesus.php
+++ tests/test-acolhesus.php
@@ -14,26 +14,28 @@ class AcolheSUSTest extends WP_UnitTestCase {
     const EMAIL_AILANA = 'ailana.lira@saude.gov.br';
     const EMAIL_DANYELLE = 'danyelle.cavalcante@saude.gov.br';
 
-    public function test_has_15_different_form_types() {
+    private $acolhe;
+
+    public function __construct() {
         global $AcolheSUS;
-        self::assertCount(self::TOTAL_FORMS, $AcolheSUS->forms);
+        $this->acolhe = $AcolheSUS;
+    }
+
+    public function test_has_15_different_form_types() {
+        self::assertCount(self::TOTAL_FORMS, $this->acolhe->forms);
     }
 
     public function test_get_mail_by_responsible() {
-        global $AcolheSUS;
-
-        $this->assertEquals(self::EMAIL_AILANA, $AcolheSUS->get_email_by_responsible('ailana'));
-        $this->assertEquals(self::EMAIL_DANYELLE, $AcolheSUS->get_email_by_responsible('danyelle'));
-        $this->assertEquals('diegop.santos@saude.gov.br', $AcolheSUS->get_email_by_responsible('diego'));
-        $this->assertEquals('', $AcolheSUS->get_email_by_responsible('giovanna'),'Deveria ter saído do projeto');
-        $this->assertEquals('', $AcolheSUS->get_email_by_responsible('marilia'), 'Deveria ter saído do projeto');
-        $this->assertEquals('', $AcolheSUS->get_email_by_responsible('janaina'),'Deveria ter saído do projeto');
+        $this->assertEquals(self::EMAIL_AILANA, $this->acolhe->get_email_by_responsible('ailana'));
+        $this->assertEquals(self::EMAIL_DANYELLE, $this->acolhe->get_email_by_responsible('danyelle'));
+        $this->assertEquals('diegop.santos@saude.gov.br', $this->acolhe->get_email_by_responsible('diego'));
+        $this->assertEquals('', $this->acolhe->get_email_by_responsible('giovanna'),'Deveria ter saído do projeto');
+        $this->assertEquals('', $this->acolhe->get_email_by_responsible('marilia'), 'Deveria ter saído do projeto');
+        $this->assertEquals('', $this->acolhe->get_email_by_responsible('janaina'),'Deveria ter saído do projeto');
     }
 
     public function test_get_forward_mail() {
-        global $AcolheSUS;
-
-        $this->assertContains(self::EMAIL_AILANA, $AcolheSUS->get_forward_mail_by_state('AM'));
-        $this->assertContains(self::EMAIL_DANYELLE, $AcolheSUS->get_forward_mail_by_state('MS'));
+        $this->assertContains(self::EMAIL_AILANA, $this->acolhe->get_forward_mail_by_state('AM'));
+        $this->assertContains(self::EMAIL_DANYELLE, $this->acolhe->get_forward_mail_by_state('MS'));
     }
 }
